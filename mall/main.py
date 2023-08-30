f4170bb08b5c570f9d4f7ef2ca908667e411f921
Tarik Yilmaz <tarikkyilmmaz@gmail.com>
2023-08-30 15:43:02 +0300

    auth and token

--- mall/main.py
+++ mall/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from mall.database import engine
 from sqlmodel import SQLModel
-from mall.routers import customer
+from mall.routers import customer, user
 
 app = FastAPI()
 app.include_router(customer.router)
+app.include_router(user.router)
 
 # Create Database and Tables on startup
 @app.on_event("startup")
--- mall/models.py
+++ mall/models.py
@@ -21,3 +21,48 @@ class ShowCustomer(SQLModel):
     CustomerID: int
     Gender: str
     Age: Optional[int]
+
+
+class Login(SQLModel):
+    username: str
+    password: str
+
+
+class User(SQLModel, table=True):
+    user_id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    username: str
+    email: str
+    password: str
+
+
+class CreateUpdateUser(SQLModel):
+    name: str
+    username: str
+    email: str
+    password: str
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "name": "Mesut KOCAMAN",
+                "username": "mlops1",
+                "email": "mlops1@vbo.local",
+                "password": "strongPassword"
+            }
+        }
+
+
+class ShowUser(SQLModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "name": "Mesut KOCAMAN",
+                "email": "mlops1@vbo.local"
+            }
+        }
